ba7757f2ce0657bfed593975cb538e6df587fdbe
rykuno <rykuno@gmail.com>
2017-09-11 18:43:24 -0500

    Signed and Podfile Created.

---
+++ Dota2ComedicStats/Dota2ComedicStats/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Dota2ComedicStats
+//
+//  Created by Donny Blaine on 9/11/17.
+//  Copyright © 2017 RyStudios. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Dota2ComedicStats/Dota2ComedicStatsTests/Dota2ComedicStatsTests.swift
@@ -0,0 +1,36 @@
+//
+//  Dota2ComedicStatsTests.swift
+//  Dota2ComedicStatsTests
+//
+//  Created by Donny Blaine on 9/11/17.
+//  Copyright © 2017 RyStudios. All rights reserved.
+//
+
+import XCTest
+@testable import Dota2ComedicStats
+
+class Dota2ComedicStatsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
